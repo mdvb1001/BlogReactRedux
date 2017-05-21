589796729d5e10e40c5838e4c7d3eabd9c6c2a42
maxvanbel <maxvanbel@gmail.com>
2017-05-21 18:51:33 -0500

    nested routes example

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-// route is used to match a url route and a react component
+// Route is used to match a url route and a react component
 
 
 import App from './components/app';
+import PostsIndex from './components/posts_index';
+
 
 export default (
-<Route path="/" component={App} />
+  <Route path="/" component={App}>
+    <IndexRoute component={PostsIndex} />
+      {/*IndexRoute doesn't need a path because it has the default path*/}
+  </Route>
 );
