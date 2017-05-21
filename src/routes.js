0b7574a101617e15510371153fd0347e5d5288fc
maxvanbel <maxvanbel@gmail.com>
2017-05-20 23:30:21 -0500

    add first route with url and component connected

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+// route is used to match a url route and a react component
+
+
+import App from './components/app';
+
+export default (
+<Route path="/" component={App} />
+);
