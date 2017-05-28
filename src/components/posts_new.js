8dad84de41fc13784b9fab12805ce3034d80c82e
maxvanbel <maxvanbel@gmail.com>
2017-05-28 16:59:07 -0500

    added 'has-danger' as a helper alert

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -24,7 +24,7 @@ class PostsNew extends Component {
 
         <h3>Create a new post</h3>
 
-        <div className="form-group">
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label>Title</label>
           <input type="text" className="form-control" {...title} />
           <div className="text-help">
@@ -36,7 +36,7 @@ class PostsNew extends Component {
           </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
           <label>categories</label>
           <input type="text" className="form-control" {...categories} />
           <div className="text-help">
@@ -44,7 +44,7 @@ class PostsNew extends Component {
           </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
           <label>Content</label>
           <textarea type="text" className="form-control" {...content} />
           <div className="text-help">
